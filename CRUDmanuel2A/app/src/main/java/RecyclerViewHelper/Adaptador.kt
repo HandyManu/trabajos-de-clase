7712e8f430966c169944c881071b205b62759717
Manuel <20220416@ricaldone.edu.sv>
2024-05-09 13:30:17 -0600

    borrar ya funciona

--- CRUDmanuel2A/app/src/main/java/RecyclerViewHelper/Adaptador.kt
+++ CRUDmanuel2A/app/src/main/java/RecyclerViewHelper/Adaptador.kt
@@ -4,12 +4,40 @@ import Manuel.Ortega.crudmanuel2_a.R
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import modelo.ClaseConexion
 import modelo.dataClassProductos
 
 class Adaptador(private var Datos: List<dataClassProductos>) : RecyclerView.Adapter<ViewHolder>() {
 
     fun actualizarLista(nuevaLista:List<dataClassProductos>){
         Datos=nuevaLista
+        notifyDataSetChanged()
+    }
+
+    fun eliminarRegistro(nombreProducto:String,position: Int){
+
+        //quitar el elementpo de la lista
+        val listaDatos = Datos .toMutableList()
+        listaDatos.removeAt(position)
+
+        //quitar de la base de datos
+        GlobalScope.launch(Dispatchers.IO) {
+
+            //crear un objeto e la clase conexion
+            val objConexion=ClaseConexion().cadenaConexion()
+
+            val deleteProducto = objConexion?.prepareStatement("delete tbproductos where NombreProducto=?")!!
+            deleteProducto.setString( 1,nombreProducto)
+            deleteProducto.executeUpdate()
+
+            val commit = objConexion.prepareStatement( "commit")!!
+            commit.executeUpdate()
+        }
+        Datos=listaDatos.toList()
+        notifyItemRemoved(position)
         notifyDataSetChanged()
 
     }
@@ -23,4 +51,11 @@ class Adaptador(private var Datos: List<dataClassProductos>) : RecyclerView.Adap
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val producto = Datos[position]
         holder.textView.text = producto.NombreProducto
-    }}
+
+        val item =Datos[position]
+        holder.imgBorrar.setOnClickListener {
+            eliminarRegistro(item.NombreProducto,position)
+        }
+
+    }
+}
